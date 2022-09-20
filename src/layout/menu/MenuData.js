b678bb5113875e1c28b67525e052b16771ae064a
Awais <muhammadawais280@gmail.com>
2022-09-20 18:25:50 +0500

    [ADD] demo4 theme and deleted some code

---
+++ src/layout/menu/MenuData.js
@@ -0,0 +1,18 @@
+const menu = [
+  { heading: "Dashboards" },
+  {
+    icon: "dashlite",
+    text: "SaaS Dashboard",
+    link: "/",
+  },
+
+  {
+    icon: "grid-alt",
+    text: "Applications",
+    active: false,
+    subMenu: [
+    ],
+  },
+
+];
+export default menu;
