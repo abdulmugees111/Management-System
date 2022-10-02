bff9433d3bff525623c4cddd253354fba4eed6c2
Awais <muhammadawais280@gmail.com>
2022-10-02 06:55:07 +0500

    [ADD] react-redux version fix

--- src/services/axios/index.js
+++ src/services/axios/index.js
@@ -2,7 +2,8 @@ import axios from 'axios'
 import store from 'store'
 
 const apiClient = axios.create({
-  baseURL: 'https://app.idara.io/api/',
+  // baseURL: 'https://app.idara.io/api/',
+  baseURL: 'http://localhost:8043/api/',
   // timeout: 1000,
   // headers: { 'X-Custom-Header': 'foobar' }
   headers: {'Content-Type': 'text/html'}
