2e3639a736b89b29b7900d10780d4557d724563e
usama-mr20 <usamamustafa89@gmail.com>
2022-10-25 21:35:58 +0500

    revamped

--- src/pages/app/subscription/OrderPage.js
+++ src/pages/app/subscription/OrderPage.js
@@ -39,7 +39,9 @@ e.preventDefault()
       <Head title="Order"></Head>
       {/* <Content size="lg"> */}
       <BlockHead>
-        <BlockHeadContent></BlockHeadContent>
+        <BlockHeadContent>
+          <BlockTitle tag="h3">Ready to get started</BlockTitle>
+        </BlockHeadContent>
       </BlockHead>
 
       <Block>
@@ -48,11 +50,11 @@ e.preventDefault()
             <Col lg="7">
               <Card
                 className="card-bordered product-card px-2 py-3 "
-                style={{ minHeight: "25rem", overflow: "visible" }}
+                style={{ overflow: "visible", background: "#fcfcfc" }}
               >
                 <h5 className="py-2 title">Product info</h5>
-                <label className="form-label" htmlFor="address">
-                  Your plan
+                <label className="form-label" htmlFor="plan">
+                  Your plan:
                 </label>
                 <RSelect
                   options={opts}
@@ -67,7 +69,7 @@ e.preventDefault()
                 <FormGroup>
                   <div className="form-label-group">
                     <label className="form-label" htmlFor="address">
-                      Mailing address
+                      Mailing address:
                     </label>
                   </div>
                   <div className="form-control-wrap">
@@ -84,8 +86,8 @@ e.preventDefault()
                 <li className="divider"></li>
 
                 <h5 className="py-2 title">Payment info</h5>
-                <label className="form-label" htmlFor="address">
-                  Select payment method
+                <label className="form-label" htmlFor="payment">
+                  Select payment method:
                 </label>
                 <RSelect
                   options={opts}
@@ -99,36 +101,33 @@ e.preventDefault()
               <Card
                 className="card-bordered product-card px-2 py-3 "
                 style={{
-                  minHeight: "25rem",
                   overflow: "visible",
-                  display: "flex",
-                  flexDirection: "column",
-                  justifyContent: "space-between",
+                  // display: "flex",
+                  // flexDirection: "column",
+                  // justifyContent: "space-between",
+                  height: "fit-content",
+                  background: "#fcfcfc",
                 }}
               >
-                <div>
-                  <h5 className=" title">Order Total</h5>
-                  <li className="divider" style={{ marginTop: "10px" }}></li>
-                  <div style={{ display: "flex", justifyContent: "space-between" }} className="px-2">
-                    <label className=" form-label">Sub total:</label>
-                    <label className=" form-label">$300</label>
-                  </div>
-                  <div style={{ display: "flex", justifyContent: "space-between" }} className="px-2">
-                    <label className=" form-label">Taxes:</label>
-                    <label className=" form-label">$300</label>
-                  </div>
-                  <li className="divider"></li>
-                  <div style={{ display: "flex", justifyContent: "space-between" }} className="px-2">
-                    <label className=" form-label">Total:</label>
-                    <label className=" form-label">$600</label>
-                  </div>
+                <h5 className=" title">Order Total</h5>
+                <li className="divider" style={{ marginTop: "10px" }}></li>
+                <div style={{ display: "flex", justifyContent: "space-between" }} className="px-2">
+                  <label className=" form-label">Sub total:</label>
+                  <label className=" form-label">$300</label>
+                </div>
+                <div style={{ display: "flex", justifyContent: "space-between" }} className="px-2">
+                  <label className=" form-label">Taxes:</label>
+                  <label className=" form-label">$300</label>
+                </div>
+                <li className="divider"></li>
+                <div style={{ display: "flex", justifyContent: "space-between" }} className="px-2">
+                  <label className=" form-label">Total:</label>
+                  <label className=" form-label">$600</label>
                 </div>
 
-                <div style={{ background: "red" }}>
-                  <Button size="lg" className="btn-block" color="primary" type="submit">
-                    Process checkout
-                  </Button>
-                </div>
+                <Button size="lg" className="btn-block mt-3" color="primary" type="submit">
+                  Process checkout
+                </Button>
               </Card>
             </Col>
           </Row>
