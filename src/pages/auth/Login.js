5fecf85c8e991a62dab4e799d72468da82ddee99
usama-mr20 <usamamustafa89@gmail.com>
2022-10-25 21:05:57 +0500

    revamp

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -143,35 +143,7 @@ const Login = ({ dispatch, user, authProvider, logo }) => {
               {" "}
               New on our platform? <Link to={`${process.env.PUBLIC_URL}/auth/register`}>Create an account</Link>
             </div>
-            {/*<div className="text-center pt-4 pb-3">*/}
-            {/*  <h6 className="overline-title overline-title-sap">*/}
-            {/*    <span>OR</span>*/}
-            {/*  </h6>*/}
-            {/*</div>*/}
-            {/*<ul className="nav justify-center gx-4">*/}
-            {/*  <li className="nav-item">*/}
-            {/*    <a*/}
-            {/*      className="nav-link"*/}
-            {/*      href="#socials"*/}
-            {/*      onClick={(ev) => {*/}
-            {/*        ev.preventDefault();*/}
-            {/*      }}*/}
-            {/*    >*/}
-            {/*      Facebook*/}
-            {/*    </a>*/}
-            {/*  </li>*/}
-            {/*  <li className="nav-item">*/}
-            {/*    <a*/}
-            {/*      className="nav-link"*/}
-            {/*      href="#socials"*/}
-            {/*      onClick={(ev) => {*/}
-            {/*        ev.preventDefault();*/}
-            {/*      }}*/}
-            {/*    >*/}
-            {/*      Google*/}
-            {/*    </a>*/}
-            {/*  </li>*/}
-            {/*</ul>*/}
+  
           </PreviewCard>
         </Block>
         <AuthFooter />
